95ec0a2a97a4c0c84135bcba4cd37f0def0b24b0
tlrollins9 <tlrollins9@gmail.com>
2020-06-30 10:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function currentLine(katzDeliLine) {
-  
+function nowServing(katzDeliLine) {
+  katzDeliLine.shift(name)
 }
