98d0e86b5a6e696cf53e082ea2670875c508db20
tlrollins9 <tlrollins9@gmail.com>
2020-06-30 10:32:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,5 @@ function nowServing(katzDeliLine) {
     }
   }
   function currentLine(katzDeliLine) {
-    if (katzDeliLine.length < 0) {
-      return "The line is currently empty."
-    } else {
-    }
+    if (katzDeliLine.length 
   }
