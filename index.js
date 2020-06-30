d3ae43d189ee5064cf9ee70b518597015eb7e0fb
tlrollins9 <tlrollins9@gmail.com>
2020-06-30 10:53:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function nowServing(katzDeliLine) {
   }
   function currentLine(katzDeliLine) {
     for (let i = 0; i < katzDeliLine.length; i++) {
-      katzDeliLine.push(`i+)
+      katzDeliLine.push(i + 1) + "." + katzDeliLine[i] +
     }
   }
